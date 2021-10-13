9780c764e66d568ab39e66773e7b9393cf02bf23
chenholy <pchen2230@gmail.com>
2021-10-14 10:43:30 +1300

    adding more unit tests

--- DollarsConverterTest/UnitTest1.cs
+++ DollarsConverterTest/UnitTest1.cs
@@ -19,12 +19,15 @@ namespace DollarsConverterTest
         }
 
         [TestCase(10, "Ten")]
+        [TestCase(21, "Twenty One")]
         public void TestDoubleDigit(int input, string expected)
         {
             Assert.AreEqual(Helpers.doubleDigit(input), expected);
         }
 
         [TestCase(1000, 4, "One Thousand")]
+        [TestCase(1000000, 7, "One Million")]
+        [TestCase(1000000000, 10, "One Billion")]
         public void TestConvertDollarToString(int num, int numLength, string expected)
         {
             Assert.AreEqual(Helpers.convertDollarString(num, numLength), expected);
